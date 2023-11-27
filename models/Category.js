b41200d736ccfd3c946a4f9e16d9549f4a75e48d
Andrew Ross <andrew.ross4224@gmail.com>
2023-11-27 16:58:14 -0500

    updated models for category and product

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,22 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { INTEGER } = require('sequelize');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+// Product.belongsTo(Category)
 // Categories have many Products
-
+// Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+// Product.belongsToMany(ProductTag)
 // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(ProductTag)
 
 module.exports = {
   Product,
